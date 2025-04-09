67a52bc974c008ec830fc1b1dd52f87a6aa1fc5f
Hayato-80 <jojohaya.fs.sb421@outlook.jp>
2025-04-10 01:09:10 +0200

    Add ros2_control

--- mobro_gazebo/launch/mobro_gazebo.launch.py
+++ mobro_gazebo/launch/mobro_gazebo.launch.py
@@ -1,43 +1,48 @@
 import os
-import launch
-import launch_ros.actions
-from launch_ros.substitutions import FindPackageShare
+
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
 from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 import xacro
 
+
 def generate_launch_description():
     # File paths for URDF and RViz configuration
-    # urdf_file = os.path.join(get_package_share_directory('mobro_description'), 'urdf', 'mobro.urdf')
-    urdf_file = os.path.join(get_package_share_directory('mobro_description'), 'urdf', 'mobro_v2.urdf')
-    xacro_file = os.path.join(get_package_share_directory('mobro_description'),'urdf','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    rviz_config_file = os.path.join(get_package_share_directory('mobro_description'), 'rviz', 'gazebo.rviz')
-    #pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    pkg_my_gazebo = get_package_share_directory('mobro_gazebo')
-    gazebo_world_file = os.path.join(pkg_my_gazebo, 'worlds', 'test_home.world')
-    #slam_params = os.path.join(pkg_my_gazebo, 'config', 'slam_params.yaml')
+    description_pkg = os.path.join(get_package_share_directory('mobro_description'))
+    gazebo_pkg = get_package_share_directory('mobro_gazebo')
+    
+    xacro_file = os.path.join(description_pkg,'urdf','robot.urdf.xacro')
+    #robot_description_config = xacro.process_file(xacro_file)
+    
+    
+    rviz_config_file = os.path.join(description_pkg, 'rviz', 'gazebo.rviz')
+    gazebo_world_file = os.path.join(gazebo_pkg, 'worlds', 'test_home.world')
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    # Declare the launch argument for GUI visibility
-    #use_gui_arg = DeclareLaunchArgument('use_gui', default_value='True', description='Use joint state publisher GUI')
-    #slam_package_dir = get_package_share_directory('slam_toolbox')
-    # gazebo_node = launch_ros.actions.Node(
-    #     package='gazebo_ros',
-    #     executable='gazebo',
-    #     name='gazebo',
-    #     arguments=['-s', 'libgazebo_ros_factory.so', gazebo_world_file]
-    # )
+    
+    #robot_description = Command(["xacro ",xacro_file])
+
+    #robot_description_config = xacro.process_file(xacro_file)
+    # robot_description_str = robot_description_config.toxml()
+    # robot_description_param = ParameterValue(robot_description_str, value_type=str)
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml(), 'use_sim_time': use_sim_time}
+
     gazebo_node = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]))
                     # launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
 
-    robot_spawn = launch_ros.actions.Node(
+    robot_spawn = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         # name='spawn_entity',
@@ -56,30 +61,59 @@ def generate_launch_description():
     #     condition=launch.conditions.IfCondition(LaunchConfiguration('use_gui'))
     # )
 
-    joint_state_publisher = launch_ros.actions.Node(
-    package='joint_state_publisher',
-    executable='joint_state_publisher',
-    name='joint_state_publisher',
-    parameters=[{'use_sim_time': use_sim_time}])
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
 
     # Robot State Publisher node
-    robot_state_publisher_node = launch_ros.actions.Node(
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description_config.toxml(),
-                      'use_sim_time': use_sim_time}]
-        #parameters=[{'robot_description': open(urdf_file).read()}]
+        parameters=[params]
     )
 
     # RViz2 node with configuration file
-    rviz2_node = launch_ros.actions.Node(
+    rviz2_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
         arguments=['-d', rviz_config_file],
         parameters=[{'use_sim_time': use_sim_time}]
+    )
+
+    joint_state_broad_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['joint_state_broad'],
+    )
+
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        arguments=['diff_drive_controller'],
+    )
+
+    # Ensure joint_state_broad_spawner starts after gazebo_node
+    joint_state_broad_after_gazebo = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_spawn,
+            on_exit=[joint_state_broad_spawner],
+        )
+    )
+
+    # Ensure diff_drive_spawner starts after joint_state_broad_spawner
+    diff_drive_spawner_after_joint_state = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_spawn,
+            on_exit=[diff_drive_spawner],
+        )
     )
 
     # Return the launch description with all the nodes and the launch argument
@@ -94,10 +128,14 @@ def generate_launch_description():
             default_value='true',
             description='Use simulation time if true'
         ),
+        
         gazebo_node,
         robot_spawn,
         #joint_state_publisher,
-        #joint_state_publisher_gui_node,
+        # joint_state_broad_spawner,
+        joint_state_broad_after_gazebo,
+        # diff_drive_spawner,
+        diff_drive_spawner_after_joint_state,
         robot_state_publisher_node,
         rviz2_node,
         # IncludeLaunchDescription(
